f866852d9f1908e2c12e1d31b3cac9f26e3abd32
Giovana Morais <giovana.vmorais@gmail.com>
2024-08-01 11:30:58 -0400

    add evidences when thesis is provided

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,6 @@
+import glob
+import os
+
 import ollama
 import streamlit as st
 
@@ -8,8 +11,8 @@ st.title("Data literacy chatbot")
 st.caption(f"A Streamlit chatbot powered by {model}")
 
 with st.sidebar:
-    # st.sidebar.title("Resources")
     "[View the source code](https://github.com/mewtyunjay/data-literacy-poc)"
+    st.sidebar.title("Evidences")
 
 st.write("Let's start! First, fill the box below with your thesis and beliefs on the topic")
 
@@ -19,6 +22,13 @@ main_topic = "Social media usage by teenagers"
 welcome_message = f"Ok, so you need to build an argument about {main_topic} and your starting point of view is \"{main_thesis}\""
 
 if main_thesis != "":
+    with st.sidebar:
+        "[View the source code](https://github.com/mewtyunjay/data-literacy-poc)"
+        st.sidebar.title("Evidences")
+        evidences = glob.glob("evidence/*.png")
+
+        for e in evidences:
+            st.image(e)
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": f"{welcome_message}. How can I help you?"}]
 
