1cf35bae61654bd7ca104365cb7e254d44bbf560
Giovana Morais <giovana.vmorais@gmail.com>
2024-07-31 23:45:55 -0400

    add welcome message

--- chatbot.py
+++ chatbot.py
@@ -12,11 +12,15 @@ with st.sidebar:
     "[View the source code](https://github.com/mewtyunjay/data-literacy-poc)"
 
 st.write("Let's start! First, fill the box below with your thesis and beliefs on the topic")
+
 main_thesis = st.text_input(label="Thesis", placeholder="Example: I believe that teenagers should not use social media because it is hurtful")
+main_topic = "Social media usage by teenagers"
+
+welcome_message = f"Ok, so you need to build an argument about {main_topic} and your starting point of view is \"{main_thesis}\""
 
 if main_thesis != "":
     if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+        st.session_state["messages"] = [{"role": "assistant", "content": f"{welcome_message}. How can I help you?"}]
 
     for msg in st.session_state.messages:
         st.chat_message(msg["role"]).write(msg["content"])
