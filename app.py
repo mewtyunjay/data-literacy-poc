3077dbd7aeddee37a53bee56811c8b0c8b8295af
giovana morais <giovana.vmorais@gmail.com>
2024-09-27 10:42:55 -0400

    Add download button to chatbot page (#7)
    * add homepage pointing to the main functionalities
    
    * remove block on list

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ st.markdown("# Welcome to the Data Remix prototype!")
 st.markdown("""
 There are two main functionalities we would like to test here:
 
-    * Evidence Selector: this tool helps the student discuss evidences with one agent and gather insights from it. Once the discussion is done, the student has the option to save it.
+* Evidence Selector: this tool helps the student discuss evidences with one agent and gather insights from it. Once the discussion is done, the student has the option to save it.
 
-    * Data Story Helper: this tool helps the student to build a coherent story from the discussed evidences.
+* Data Story Helper: this tool helps the student to build a coherent story from the discussed evidences.
 
 You can access both on the links below.
 """)
